5045419d57c54ca382b929dfc318bd26ac58c124
Kevin Wilkinson <willkillson@gmail.com>
2020-03-30 20:59:00 -0700

    Adds boiler plate for ManageUsers class.

--- src/main/java/memoranda/gym/ManageUsers.java
+++ src/main/java/memoranda/gym/ManageUsers.java
@@ -1,4 +1,38 @@
 package main.java.memoranda.gym;
 
+import java.util.HashMap;
+
 public class ManageUsers {
+    HashMap<String, User> users;
+
+    public ManageUsers(){
+        this.users = new HashMap<>();
+    }
+
+    public boolean createUser(String userName, String userPassword){
+        //TODO
+        return false;
+    }
+
+    public boolean setPassword(String email, String password){
+        //TODO
+        return false;
+    }
+
+    boolean assignBelt(String email, String password){
+        //TODO
+        return false;
+    }
+
+    public boolean assignTrainingRank(String email, Belt belt){
+        //TODO
+        return false;
+    }
+
+    public User getUser(String email){
+        //TODO
+        return null;
+    }
+
+
 }

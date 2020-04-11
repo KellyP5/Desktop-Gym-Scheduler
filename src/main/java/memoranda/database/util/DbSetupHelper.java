0c39c3fff66638de8cc7cb331938d752b6e589c8
Kevin Somers <ksomers3@asu.edu>
2020-04-11 19:44:43 -0400

    Update GYMCLASS Id to autoincrement, add some additional comments, factor out constants to SqlConstants

--- src/main/java/memoranda/database/util/DbSetupHelper.java
+++ src/main/java/memoranda/database/util/DbSetupHelper.java
@@ -1,11 +1,6 @@
 package main.java.memoranda.database.util;
 
-import main.java.memoranda.database.RoleEntity;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DbSetupHelper {
 
@@ -15,29 +10,29 @@ public class DbSetupHelper {
         dbSetupHelper.createDatabase();
         dbSetupHelper.createNeujahrskranzTables();
         //create test db
-        dbSetupHelper.setUrl(dbSetupHelper.defaultTestLoc);
+        dbSetupHelper.setDbURL(SqlConstants.defaultTestDbLoc);
         dbSetupHelper.createDatabase();
         dbSetupHelper.createNeujahrskranzTables();
+        //add sample data to test
+        dbSetupHelper.addSampleData();
     }
 
-    public final String defaultLoc = "jdbc:sqlite:src\\main\\resources\\database\\real.db";
-    public final String defaultTestLoc = "jdbc:sqlite:src\\main\\resources\\database\\test.db";
-    private String url;
+    private String dbURL;
 
     public DbSetupHelper() {
-        this.url = defaultLoc;
+        this.dbURL = SqlConstants.defaultDbLoc;
     }
-    public DbSetupHelper(String url) {
-        this.url = url;
+    public DbSetupHelper(String dbURL) {
+        this.dbURL = dbURL;
     }
-    public String getUrl() {
-        return url;
+    public String getDbURL() {
+        return dbURL;
     }
-    public void setUrl(String url) {
-        this.url = url;
+    public void setDbURL(String dbURL) {
+        this.dbURL = dbURL;
     }
     public void createDatabase(){
-        try (Connection conn = DriverManager.getConnection(url)) {
+        try (Connection conn = DriverManager.getConnection(dbURL)) {
             if (conn != null) {
                 System.out.println("A new database has been created.");
             }
@@ -47,8 +42,7 @@ public class DbSetupHelper {
     }
     public void closeDatabase(){
         try {
-            Connection conn = null;
-            conn = DriverManager.getConnection(url);
+            Connection conn = DriverManager.getConnection(dbURL);
             conn.close();
         } catch (SQLException e){
             System.out.println(e.getMessage());
@@ -56,8 +50,8 @@ public class DbSetupHelper {
         }
     }
     public void createTable(String sql, String tableName){
-        try (Connection conn = DriverManager.getConnection(url);
-            Statement stmt = conn.createStatement()) {
+        try (Connection conn = DriverManager.getConnection(dbURL);
+             Statement stmt = conn.createStatement()) {
             stmt.execute(sql);
             System.out.println("Table: " + tableName + " was created.");
         } catch (SQLException e) {
@@ -67,8 +61,9 @@ public class DbSetupHelper {
     public void createNeujahrskranzTables(){
         createDatabase();
         System.out.println("Preparing to create tables..note that if the tables already exist they will not be created");
+        //create USER
         String userSql = "CREATE TABLE IF NOT EXISTS USER (\n"
-                + "    Email text PRIMARY KEY,\n"
+                + "    Email text PRIMARY KEY NOT NULL,\n"
                 + "    FirstName text NOT NULL,\n"
                 + "    LastName text NOT NULL,\n"
                 + "    Password text NOT NULL,\n"
@@ -77,8 +72,10 @@ public class DbSetupHelper {
                 + "    TrainingBelt text\n"
                 + ");";
         createTable(userSql, "User");
+
+        //create GYMCLASS
         String classSql = "CREATE TABLE IF NOT EXISTS GYMCLASS (\n"
-                + "    Id integer PRIMARY KEY,\n"
+                + "    Id integer PRIMARY KEY AUTOINCREMENT,\n"
                 + "    RoomNumber integer NOT NULL,\n"
                 + "    StartDate text NOT NULL,\n"
                 + "    StartTime real NOT NULL,\n"
@@ -91,16 +88,20 @@ public class DbSetupHelper {
                 + "    FOREIGN KEY(CreatedByEmail) REFERENCES USER(Email) ON DELETE CASCADE\n"
                 + ");";
         createTable(classSql, "GymClass");
+
+        //create TRAINERAVAILABILITY
         String trainerAvailSql = "CREATE TABLE IF NOT EXISTS TRAINERAVAILABILITY (\n"
-                + "    TrainerEmail text PRIMARY KEY,\n"
+                + "    TrainerEmail text PRIMARY KEY NOT NULL,\n"
                 + "    StartDate text NOT NULL,\n"
                 + "    StartTime real NOT NULL,\n"
                 + "    EndTime real NOT NULL,\n"
                 + "    FOREIGN KEY(TrainerEmail) REFERENCES USER(Email) ON DELETE CASCADE\n"
                 + ");";
         createTable(trainerAvailSql, "TrainerAvailability");
+
+        //create ENROLLEDUSER
         String enrolledUsersSql = "CREATE TABLE IF NOT EXISTS ENROLLEDUSER (\n"
-                + "    ClassId integer PRIMARY KEY,\n"
+                + "    ClassId integer PRIMARY KEY NOT NULL,\n"
                 + "    UserEmail text NOT NULL,\n"
                 + "    FOREIGN KEY(UserEmail) REFERENCES USER(Email) ON DELETE CASCADE\n"
                 + "    FOREIGN KEY(ClassId) REFERENCES GYMCLASS(Id) ON DELETE CASCADE\n"
@@ -109,10 +110,5 @@ public class DbSetupHelper {
     }
     public void addSampleData(){
         //TODO
-    }
-
-    private void insertUser(String email, String firstName, String lastName, String password, RoleEntity role){
-        //TODO
-
     }
 }
---
+++ src/main/java/memoranda/database/util/SqlConstants.java
@@ -0,0 +1,6 @@
+package main.java.memoranda.database.util;
+
+public class SqlConstants {
+    public static final String defaultDbLoc = "jdbc:sqlite:src\\main\\resources\\database\\real.db";
+    public static final String defaultTestDbLoc = "jdbc:sqlite:src\\main\\resources\\database\\test.db";
+}

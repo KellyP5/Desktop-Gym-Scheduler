9038d41333a2e06a9c192ec095f9bc2c33ac3fc1
Kevin Somers <ksomers3@asu.edu>
2020-04-12 14:02:10 -0400

    test database now goes to src\test\resources\database\test.db

--- src/main/java/memoranda/database/util/SqlConstants.java
+++ src/main/java/memoranda/database/util/SqlConstants.java
@@ -6,5 +6,5 @@ Constants to be used for database related information
 public class SqlConstants {
     public static final String DEFAULTDBLOC = "jdbc:sqlite:src\\main\\resources\\database\\real.db";
     public static final String DEFAULTTESTDBLOC =
-            "jdbc:sqlite:src\\main\\resources\\database\\test.db";
+            "jdbc:sqlite:src\\test\\resources\\database\\test.db";
 }

41c61aba633dee584759aa48ad08fa52038e1e79
Kevin Wilkinson <willkillson@gmail.com>
2020-04-13 03:01:03 -0700

    Merges complete

--- src/main/java/memoranda/ui/App.java
+++ src/main/java/memoranda/ui/App.java
@@ -41,13 +41,8 @@ public class App {
      */
     public static final String WEBSITE_URL = "http://globogym.com";
 
-<<<<<<< HEAD
+
     public static SqlConnection conn = null;
-
-
-=======
-    public static SqlConnection connection = null;
->>>>>>> 1073c1eca4410d2eb66e8f49b4c5ffe5dd9138c7
 
 	private JFrame splash = null;
 	FileInputStream input;

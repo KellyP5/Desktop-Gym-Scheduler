8a5ebb1f88c5cda0870e241c5dd314d17693f14e
mbarrill <mbarrill@asu.edu>
2020-04-29 19:48:27 -0700

    test commit

--- src/main/java/memoranda/ui/classes/ClassesSetAvailability.java
+++ src/main/java/memoranda/ui/classes/ClassesSetAvailability.java
@@ -154,7 +154,7 @@ public class ClassesSetAvailability extends JFrame {
     public void availabilitySetup () throws SQLException {
 
 
-
+        //test comment
         String userEmail = extractTrainerEmail();
         double start = Local.getDoubleTime(startTimeCB.getSelectedItem().toString());
         double end = Local.getDoubleTime(endTimeCB.getSelectedItem().toString());
